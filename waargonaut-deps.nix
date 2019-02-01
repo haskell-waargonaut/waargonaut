bd4d3e5900ce67f7473529e4ab0cecdb27bdf3a8
Sean Chalmers <sclhiannan@gmail.com>
2019-02-01 18:16:17 +1000

    Bump generics-sop to v4.
    This means that waargonaut will no longer build with GHC 7.10.3.
    
    Updated `tested-with` and the `.travis.yml`.

--- waargonaut-deps.nix
+++ waargonaut-deps.nix
@@ -3,6 +3,8 @@ self: super: {
     overrides = self.lib.composeExtensions (old.overrides or (_: _: {})) (hself: hsuper: {
       hw-json   = self.haskell.lib.dontBenchmark (hself.callHackage "hw-json" "0.9.0.1" {});
       hw-parser = hself.callHackage "hw-parser" "0.1.0.0" {};
+      sop-core = hself.callHackage "sop-core" "0.4.0.0" {};
+      generics-sop = hself.callHackage "generics-sop" "0.4.0.1" {};
     });
   });
 }
